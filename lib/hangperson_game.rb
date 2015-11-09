35226d40ed8713466891b64101f8e20fc9b19308
Marco A <olveram11@gmail.com>
2015-11-09 16:58:33 -0600

    First part ready

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -7,11 +7,53 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
+  attr_accessor :word, :guesses, :wrong_guesses
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses= ''
   end
 
+  def guess(s)
+    raise ArgumentError, "Please enter a valid letter" if s == nil or s.empty? or s.match(/\W/)
+    s.downcase!
+    if @word.include? s
+      if @guesses.include? s
+        return false
+      else
+        @guesses << s
+      end
+    else
+      if @wrong_guesses.include? s
+        return false
+      else
+        @wrong_guesses << s
+      end
+    end
+  end
+  
+  def word_with_guesses
+    result = ''
+    @word.each_char { |c|
+      if @guesses.include? c
+        result << c
+      else
+        result << '-'
+      end      
+    }
+    return result
+  end
+  
+  def check_win_or_lose
+    if not self.word_with_guesses.include? '-' and not self.word_with_guesses.empty?
+      return :win
+    elsif (@guesses.size + @wrong_guesses.size) < 7
+      return :play
+    else
+      return :lose      
+    end
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
